1d26532c8519ebdc51661e9aba1f307238ce1799
soujanya.narlapuram <soujanya131narlapuram@gmail.com>
2023-11-06 11:33:51 +0530

    merge conflict initial commit

---
+++ src/main/java/SampleTest.java
@@ -0,0 +1,8 @@
+public class SampleTest {
+    public static void main(String[] args) {
+        System.out.println("hello ,welcome to git sample");
+    }
+    public static void PrintSomeCode(){
+        System.out.println("get to code");
+    }
+}
